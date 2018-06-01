ea6f2406b43d23c4b700e36a914f540f30bd09bb
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-06-01 14:56:06 -0700

    Small style changes and doc fixes

--- python/lsst/dax/webservcommon/auth.py
+++ python/lsst/dax/webservcommon/auth.py
@@ -1,3 +1,27 @@
+# LSST Data Management System
+# Copyright 2018 AURA/LSST.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+
+"""
+Authentication decorators for webserv Flask applications
+"""
+
 import json
 from functools import wraps
 
@@ -14,17 +38,23 @@ class DaxAuthException(Exception):
 
 
 class JwtAuth:
+    """JwtAuth is a thin wrapper around the PyJWT project.
+    Parameters are best documented in that project.
+
+    Parameters
+    ----------
+    key : RSAPublicKey
+        Public key to verify RS256 signatures with
+    issuer : str
+        Domain of issuer, if applicable
+    audience : str
+        Audience of token, if applicable. If this is false or None,
+        then the check is turned off
+    options : dict
+        Verification options to pass to PyJWT's decode function
+    """
 
     def __init__(self, key, issuer=None, audience=None, options=None):
-        """
-        JwtAuth is a thin wrapper around the PyJWT project. Parameters
-        are best documented in that project.
-        :param key: Public key to verify RS256 signatures with
-        :param issuer: Domain of issuer, if applicable
-        :param audience: Audience of token, if applicable. If this is
-        false, then the check is turned off
-        :param options: verification options to pass to decode
-        """
         if key is None:
             raise DaxAuthException("Need a valid public key for initialization")
         self.key = key
@@ -35,7 +65,18 @@ class JwtAuth:
             self.options["verify_aud"] = False
 
     def jwt_required(self):
-        """View decorator that requires a valid JWT token and adds"""
+        """Flask decorator that requires a valid JWT token.
+        Failures to authenticate raises exceptions.
+        The REMOTE_USER request environment var will be set as well as
+        valid JWT stored on success.
+
+        Raises
+        ------
+        DaxAuthException
+            If the Authorization header is missing or token is incorrect.
+        InvalidTokenError
+            If the token is invalid in some way
+        """
 
         def wrapper(fn):
             @wraps(fn)
@@ -48,12 +89,13 @@ class JwtAuth:
         return wrapper
 
     def _validate(self):
-        header = request.headers.get('Authorization', None)
+        """Validate the token in the Flask request"""
+        header = request.headers.get("Authorization", None)
         if not header:
             raise DaxAuthException("No Auth header")
         parts = header.split()
 
-        if not parts or parts[0].lower() != "bearer" or len(parts) != 2:
+        if len(parts) != 2 or parts[0].lower() != "bearer":
             raise DaxAuthException("Not a bearer token")
 
         token = parts[1]
