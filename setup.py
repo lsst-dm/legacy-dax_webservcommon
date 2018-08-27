56f5cbae737ba500790ea22990bbd3e8a2913022
Christine Banek <cbanek@lsst.org>
2018-08-27 15:52:06 -0700

    [DM-15396] Update to new packaging way.
    Use the setup.py to create a version, and use it in version.py.
    Setup requirements.txt to handle dependencies.
    Run tests with python setup.py test

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup
 
-requires = [
-    'flask',
-    'jinja2'
-]
-
 setup(
     name='webservcommon',
-    version='0.1',
     package_dir={'lsst': 'python/lsst'},
     package_data={'lsst': ['dax/webservcommon/templates/*.html']},
     packages=['lsst', 'lsst.dax.webservcommon'],
     zip_safe=False,
-    install_requires=requires
+    use_scm_version=True
 )
